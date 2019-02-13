180beb155df4e763db66e22e373a77e844481edb
racsoraul <racsoraul@gmail.com>
2019-02-13 11:34:39 -0600

    Bash script for downloading a UE4 environment.

---
+++ getDefaultEnv.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# UE4 environment
+ENVIRONMENT_URL=https://github.com/VertexStudio/UnrealGAMS/releases/download/v0.1/GrassMountains.zip
+
+if [ ! -d "$(pwd)/Content" ]; then
+    echo "ERROR! You must execute command in the root of UE4 project."
+    echo "If you are in the right place, make sure the 'Content' folder exists."
+    echo "Exiting."
+    exit
+fi
+
+echo "Downloading..."
+wget "$ENVIRONMENT_URL" -O environment.zip
+mv ./environment.zip ./Content
+pushd ./Content
+unzip environment.zip
+rm environment.zip
+popd
+
+echo
+echo -n $'\u2714'; echo " Environment successfully added."
+echo
