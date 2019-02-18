058e9080678be912e2b6f2ae031153f552754bb0
rozgo <alex.rozgo@gmail.com>
2019-02-18 12:42:04 -0600

    fixes run_game script

--- run_game.sh
+++ run_game.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-"$UE4_ROOT"/Engine/Binaries/Linux/UE4Editor "$UE4_GAMS"/Simbotic.uproject -windowed -game -ResX=1024 -ResY=576 $@
+"$UE4_ROOT"/Engine/Binaries/Linux/UE4Editor "$UE4_GAMS"/UnrealGAMS.uproject -windowed -game -ResX=1024 -ResY=576 $@
