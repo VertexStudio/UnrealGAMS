7a83842365a917c702dad2fc2e821cc260cc9ef8
rozgo <alex.rozgo@gmail.com>
2019-03-04 14:27:03 -0600

    plugin cleaup

--- Plugins/DynamicalSystems/Source/DynamicalSystems/Private/DynamicsCommon.cpp
+++ Plugins/DynamicalSystems/Source/DynamicalSystems/Private/DynamicsCommon.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "DynamicsCommon.h"
 
 float UDynamicsCommon::MeanOfFloatArray(const TArray<float> &Samples)
