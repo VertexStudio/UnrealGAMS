0ae61f0c509710a21a57641d97e806a99c7b6949
Raúl Pleitez <racsoraul@gmail.com>
2019-04-08 19:12:08 -0600

    Debug scripts.

---
+++ debug_build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "----------------------"
+make UnrealGAMSEditor-Linux-DebugGame
---
+++ debug_game.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+$UE4_GAMS/debug_editor.sh -game -windowed -ResX=1024 -ResY=576 $@
+
